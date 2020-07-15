9bc280d54874c41c4adba61b1b0406cd55c36c82
YXL <chenxin.lan.76@gmail.com>
2020-07-15 19:52:52 +0800

    ✨feat(args): add `--port`

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+#[macro_use]
+extern crate clap;
+
 fn main() {
-    println!("Hello, world!");
+    let matches = clap_app!(myapp =>
+        (@arg PORT: --port +takes_value "Sets listening port")
+    )
+    .get_matches();
+
+    let port = matches.value_of("PORT").unwrap_or("19260");
+    println!("Value for port: {}", port);
 }
